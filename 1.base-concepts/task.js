ca9dbf3dabeb8566b031a9e5951eab0caee04d22
Anna Kainova <annakainova88@gmail.com>
2023-07-08 16:26:32 +0200

    done both tasks

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,25 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
   let arr = [];
-  
+
+  const discriminant = Math.pow(b, 2) - 4 * a * c;
+
+  if (discriminant === 0) {
+    arr.push(-b / (2 * a));
+  } else if (discriminant > 0) {
+    arr.push((-b + Math.sqrt(discriminant)) / (2 * a));
+    arr.push((-b - Math.sqrt(discriminant)) / (2 * a));
+  }
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let monthRate = percent / 100 / 12;
+  let loan = amount - contribution;
+  let sumForMonth =
+    loan * (monthRate + monthRate / (Math.pow(1 + monthRate, countMonths) - 1));
+  let totalAmount = sumForMonth * countMonths;
+
+  return +totalAmount.toFixed(2);
 }
