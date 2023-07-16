4fbd88925e0694cf661029c5b44db3cfa6620bef
Anna Kainova <annakainova88@gmail.com>
2023-07-16 18:46:17 +0200

    all done

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,34 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
-}
+  this.subject = subjectName;
+};
 
 Student.prototype.addMarks = function (...marks) {
-  
-}
+  if (this?.marks) {
+    this.marks = [...this.marks, ...marks];
+  }
+};
 
 Student.prototype.getAverage = function () {
-  
-}
+  let sum = 0;
+  if (this?.marks && this.marks.length !== 0) {
+    for (let i = 0; i < this.marks.length; i++) {
+      sum += this.marks[i];
+    }
+  } else {
+    return 0;
+  }
+  return sum / this.marks.length;
+};
 
 Student.prototype.exclude = function (reason) {
-  
-}
+  delete this.marks;
+  delete this.subject;
+  this.excluded = reason;
+};
